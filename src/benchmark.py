e1a9633fb35a06889176a4143223657c23702c88
dhruvupreti05 <dhruvupreti05@gmail.com>
2026-05-22 12:51:03 -0400

    changes

---
+++ src/benchmark.py
@@ -0,0 +1,19 @@
+"""
+benchmarking community biased asset allocation.
+"""
+
+import numpy as np
+import pandas as pd
+import networkx as nx
+
+import os
+import numpy as np
+from dotenv import load_dotenv
+
+from modules import *
+
+def asset_allocation():
+    print(":)")
+
+if __name__ == "__main__":
+    print(":)")
